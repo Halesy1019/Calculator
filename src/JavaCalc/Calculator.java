aab9416855ffc23ff63c46ae67e26defd8b09738
Haley Brightwell <hbrightwell1101@gmail.com>
2022-11-30 18:18:59 -0600

    Add variables and commands

---
+++ src/JavaCalc/Calculator.java
@@ -0,0 +1,20 @@
+package JavaCalc;
+
+public class Calculator {
+	public int add(int num1, int num2) {
+		return num2;
+		}
+	public int subtract(int num1, int num2) {
+		return num2;
+	}
+	public int multiply(int num1, int num2) {
+		return num2;
+	}
+	public double divide(int num1, int num2) {
+		return num2;
+	}
+	public int square(int num1, int num2) {
+		return num2;
+	}
+
+}
