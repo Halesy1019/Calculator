5b91010b60ef7d91dfc04f6826d46c75ab7381a1
Haley Brightwell <hbrightwell1101@gmail.com>
2022-12-04 15:41:57 -0600

    Add test output for addtion method

--- src/JavaCalc/Calculator.java
+++ src/JavaCalc/Calculator.java
@@ -1,19 +1,23 @@
 package JavaCalc;
 
 public class Calculator {
-	public int add(int num1, int num2) {
-		return num2;
-		}
-	public int subtract(int num1, int num2) {
+	public static int add(int num1, int num2) {
 		return num2;
 	}
-	public int multiply(int num1, int num2) {
+
+	public static int subtract(int num1, int num2) {
 		return num2;
 	}
-	public double divide(int num1, int num2) {
+
+	public static int multiply(int num1, int num2) {
 		return num2;
 	}
-	public int square(int num1, int num2) {
+
+	public static double divide(int num1, int num2) {
+		return num2;
+	}
+
+	public static int square(int num1, int num2) {
 		return num2;
 	}
 
--- src/JavaCalc/JavaCalcHW.java
+++ src/JavaCalc/JavaCalcHW.java
@@ -3,7 +3,20 @@ package JavaCalc;
 public class JavaCalcHW {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Welcome to the calculator.");
+		System.out.println("This is the addition method:");
+		int add1=3, add2=15;
+		int addResult;
+		System.out.println("Here are the numbers I'm about to add together:");
+		System.out.printf("Number 1: %d, number 2: %d%n", add1, add2);
+		System.out.printf("Expected result is: %d%n", (int)(add1 + add2));
+		addResult= Calculator.add(add1, add2);
+		System.out.printf("Actual result: %d%n", addResult);
+		System.out.printf("Actual result matches expected result: %s%n", (addResult==(add1 + add2)));
+		System.out.println("This is the subtraction method:");
+		System.out.println("This is the multiplication method:");
+		System.out.println("This is the division method:");
+		System.out.println("This is the square method:");
 
 	}
 
